a25104fadcae44909b35fd33fde6b815e9ae71c1
kicap1992 <kicap.karan92@gmail.com>
2021-12-21 00:25:43 +0800

    added admin interface

---
+++ admin/assets/js/index.js
@@ -0,0 +1,14 @@
+$(function() {
+    "use strict";
+
+    
+
+
+    // notification popup
+    toastr.options.closeButton = true;
+    toastr.options.positionClass = 'toast-bottom-right';
+    toastr.options.showDuration = 1000;
+    toastr['info']('Hello, welcome to HexaBit, a unique admin Template.');
+
+    
+});
--- admin/readme.md
+++
@@ -1,7 +0,0 @@
-Aplikasi Track Stok Barang dan Penjualan
-Ini terkhusus untuk admin (Penjualan, Stock masuk/keluar)
-
-
-******************************************************
-
-Created By Kicap Karan www.facebook.com/kicap.karan
