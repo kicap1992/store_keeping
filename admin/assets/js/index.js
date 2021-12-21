f64e1260a7b00e22873352b3fc87df54192af68e
kicap1992 <kicap.karan92@gmail.com>
2021-12-22 04:28:54 +0800

    added few .html files

--- admin/assets/js/index.js
+++ admin/assets/js/index.js
@@ -1,14 +1,8 @@
 $(function() {
     "use strict";
-
-    
-
-
     // notification popup
     toastr.options.closeButton = true;
     toastr.options.positionClass = 'toast-bottom-right';
     toastr.options.showDuration = 1000;
-    toastr['info']('Hello, welcome to HexaBit, a unique admin Template.');
-
-    
+    toastr['info']('Hello, Selamat Datang Kembali, Aplikasi Dibuat Oleh Kicap Karan.');
 });
