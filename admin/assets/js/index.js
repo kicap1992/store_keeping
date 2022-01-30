61263cc0ff1631ef20b208efdbc3a755a855d467
kicap1992 <kicap.karan92@gmail.com>
2022-01-31 04:50:46 +0800

    delete admin folder

--- admin/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    "use strict";
-    // notification popup
-    toastr.options.closeButton = true;
-    toastr.options.positionClass = 'toast-bottom-right';
-    toastr.options.showDuration = 1000;
-    toastr['info']('Hello, Selamat Datang Kembali, Aplikasi Dibuat Oleh Kicap Karan.');
-});
